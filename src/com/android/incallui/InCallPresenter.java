da41b22aae83ffaa446f9d97299cb2b5eb50867a
Lars Greiss <kufikugel@googlemail.com>
2015-12-26 17:37:37 +0100

    Flip to Mute/Reject Call [3/3]
    Ported over from SlimKat implementation (thanks Kufi)
    
    Original message in 4.4:
    Ported over from 4.2 implementation (thanks sasikumardr for that)
        Original author of this patch was bugadani
        We did just a cleanup
        Provide easy mute and dismiss functionality for the phone
        application.
        Available actions:
        0: mute ringer
        1: dismiss call
        2: no action (default)

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -97,6 +97,7 @@ public class InCallPresenter implements CallList.Listener,
     private CallList mCallList;
     private InCallActivity mInCallActivity;
     private InCallState mInCallState = InCallState.NO_CALLS;
+    private AccelerometerListener mAccelerometerListener;
     private ProximitySensor mProximitySensor;
     private boolean mServiceConnected = false;
     private boolean mAccountSelectionCancelled = false;
@@ -237,6 +238,8 @@ public class InCallPresenter implements CallList.Listener,
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
                 PowerManager.ACQUIRE_CAUSES_WAKEUP, "InCallPresenter");
+
+        mAccelerometerListener = new AccelerometerListener(context);
 
         mCallList = callList;
 
@@ -472,6 +475,10 @@ public class InCallPresenter implements CallList.Listener,
         Log.i(this, "Phone switching state: " + oldState + " -> " + newState);
         mInCallState = newState;
 
+        if (!newState.isIncoming() && mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
+
         // notify listeners of new state
         for (InCallStateListener listener : mListeners) {
             Log.d(this, "Notify " + listener + " of state " + mInCallState.toString());
@@ -500,6 +507,10 @@ public class InCallPresenter implements CallList.Listener,
 
         Log.i(this, "Phone switching state: " + oldState + " -> " + newState);
         mInCallState = newState;
+
+        if (newState.isIncoming() && mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(true);
+        }
 
         for (IncomingCallListener listener : mIncomingCallListeners) {
             listener.onIncomingCall(oldState, mInCallState, call);
@@ -720,6 +731,11 @@ public class InCallPresenter implements CallList.Listener,
      * Answers any incoming call.
      */
     public void answerIncomingCall(Context context, int videoState) {
+        // Just in case
+        if (mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
+
         // By the time we receive this intent, we could be shut down and call list
         // could be null.  Bail in those cases.
         if (mCallList == null) {
@@ -738,6 +754,11 @@ public class InCallPresenter implements CallList.Listener,
      * Declines any incoming call.
      */
     public void declineIncomingCall(Context context) {
+        // Just in case
+        if (mAccelerometerListener != null) {
+            mAccelerometerListener.enableSensor(false);
+        }
+
         // By the time we receive this intent, we could be shut down and call list
         // could be null.  Bail in those cases.
         if (mCallList == null) {
@@ -946,6 +967,9 @@ public class InCallPresenter implements CallList.Listener,
         if (incomingCall != null) {
             TelecomAdapter.getInstance().answerCall(
                     incomingCall.getId(), VideoProfile.STATE_AUDIO_ONLY);
+            if (mAccelerometerListener != null) {
+                mAccelerometerListener.enableSensor(false);
+            }
             return true;
         }
 
@@ -1363,10 +1387,9 @@ public class InCallPresenter implements CallList.Listener,
                 mProximitySensor.tearDown();
             }
             mProximitySensor = null;
-
             mWakeLock = null;
             mPowerManager = null;
-
+            mAccelerometerListener = null;
             mAudioModeProvider = null;
 
             if (mStatusBarNotifier != null) {
